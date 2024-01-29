407b9b910848c6bcea63385d0093aea030ca2a29
AndreyMakhankov <andrey.makhankov@yandex.ru>
2024-01-29 15:29:08 +0300

    выгрузка из облака

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-# This is a sample Python script.
+import gdown
+import zipfile
+import os
+from PIL import Image
+import random
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    gdown.download('https://storage.yandexcloud.net/aiueducation/Content/base/l5/middle_fmr.zip', None, quiet=True)
+    with zipfile.ZipFile('middle_fmr.zip', 'r') as zip_ref:
+        zip_ref.extractall('content')
+    imagePath = 'content'
+    classList = sorted(os.listdir(imagePath))
+    classCount = len(classList)
+    fig, axs = plt.subplots(1, classCount, figsize=(25, 5))
+    for i in range(classCount):
+        car_path = f'{imagePath}/{classList[i]}/'
+        img_path = car_path + random.choice(os.listdir(car_path))
+        axs[i].set_title(classList[i])
+        axs[i].imshow(Image.open(img_path))
+        axs[i].axis('off')
+    plt.show()
